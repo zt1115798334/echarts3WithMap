ef04e929ea4ed418a2d274d90dabfbb42cbe9e98
zhangtong <zhangtong9498@qq.com>
2018-02-07 13:58:47 +0800

    添加贵州信息

--- demo/static/js/guizhou.js
+++ demo/static/js/guizhou.js
@@ -76,7 +76,7 @@ var option = {
             var value = (params.value + '').split('.');
             value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
             value2 = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-            return params.seriesName + '<br/>' + params.name + ': ' + value + ":" + value2;
+            return params.name + '<br/>' + params.name + ': ' + value + ":" + value2;
         }
     },
     toolbox: {
